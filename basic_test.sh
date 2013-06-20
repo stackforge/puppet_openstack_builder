512fa012875c707520a51aa16328e8656448c20b
Dan Bode <bodepd@gmail.com>
2013-06-20 15:12:08 -0700

    Explicitly exit from vagrant ssh commands.
    I have found that commands executed from vagrant
    via ssh often hang and that an exiplicit exit
    will allow the commands to complete.
    
    This commit adds exit $? to the end of all of
    these remotely executed commands.

--- basic_test.sh
+++ basic_test.sh
@@ -7,17 +7,17 @@ vagrant destroy compute_basevm -f
 vagrant up build 2>&1 | tee -a build.log.$datestamp  
 vagrant up control_basevm 2>&1 | tee -a control.log.$datestamp
 vagrant up compute_basevm 2>&1 | tee -a compute.log.$datestamp
-vagrant ssh build -c 'sudo /tmp/test_nova.sh'
-vagrant ssh build -c 'ping -c 2 172.16.2.129'
+vagrant ssh build -c 'sudo /tmp/test_nova.sh;exit $?'
+vagrant ssh build -c 'ping -c 2 172.16.2.129;exit $?'
 
 if [ $? -eq 0 ]
   then 
     echo "##########################"
     echo "      Test Passed!"
     echo "OVS ON CONTROL:" >> control.log.$datestamp
-    vagrant ssh control_basevm -c 'sudo ovs-vsctl show' >> control.log.$datestamp
+    vagrant ssh control_basevm -c 'sudo ovs-vsctl show;exit $?' >> control.log.$datestamp
     echo "OVS ON COMPUTE:" >> compute.log.$datestamp
-    vagrant ssh compute_basevm -c 'sudo ovs-vsctl show' >> compute.log.$datestamp
+    vagrant ssh compute_basevm -c 'sudo ovs-vsctl show;exit $?' >> compute.log.$datestamp
     mv build.log.$datestamp build.log.$datestamp.success
     mv control.log.$datestamp control.log.$datestamp.success
     mv compute.log.$datestamp compute.log.$datestamp.success
@@ -26,13 +26,13 @@ else
     echo "##########################"
     echo "Ping failed to reach VM :("
     echo "OVS ON CONTROL:" >> control.log.$datestamp
-    vagrant ssh control_basevm -c 'sudo ovs-vsctl show' >> control.log.$datestamp
+    vagrant ssh control_basevm -c 'sudo ovs-vsctl show;exit $?' >> control.log.$datestamp
     echo "OVS ON COMPUTE:" >> compute.log.$datestamp
     vagrant ssh compute_basevm -c 'sudo ovs-vsctl show' >> compute.log.$datestamp
     vagrant ssh control_basevm -c 'sudo service quantum-plugin-openvswitch-agent restart'
     sleep 2
     echo "OVS ON CONTROL AFTER AGENT RESTART:" >> control.log.$datestamp
-    vagrant ssh control_basevm -c 'sudo ovs-vsctl show' >> control.log.$datestamp
+    vagrant ssh control_basevm -c 'sudo ovs-vsctl show;exit $?' >> control.log.$datestamp
     mv build.log.$datestamp build.log.$datestamp.failed
     mv control.log.$datestamp control.log.$datestamp.failed
     mv compute.log.$datestamp compute.log.$datestamp.failed

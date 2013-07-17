c9747d1078a30ae5e6462d4bbc30b0e119a9edb2
Pradeep Kilambi <pkilambi@cisco.com>
2013-07-17 13:28:15 -0400

    check to invoke cobbler definitions

--- manifests/site.pp
+++ manifests/site.pp
@@ -9,21 +9,24 @@ node build-server {
   # I want these nodes to eventuall be moved to
   # some data format that can just be parsed
   # right now, it does make sense to maintain them here
-  coi::cobbler_node { "control-server":
-    node_type      => "control",
-    mac            => "00:11:22:33:44:55",
-    ip             => "192.168.242.10",
-    power_address  => "192.168.242.110",
-    power_user     => "admin",
-    power_password => "password",
-    power_type     => "ipmitool"
-  }
+  # TODO: check the hiera's enable_cobbler here instead
+  if ($::osfamily == 'Debian') {
+    coi::cobbler_node { "control-server":
+      node_type      => "control",
+      mac            => "00:11:22:33:44:55",
+      ip             => "192.168.242.10",
+      power_address  => "192.168.242.110",
+      power_user     => "admin",
+      power_password => "password",
+      power_type     => "ipmitool"
+    }
 
-  coi::cobbler_node { "compute-server01":
-    node_type     => "compute",
-    mac           => "11:22:33:44:55:66",
-    ip            => "192.168.242.21",
-    power_address => "192.168.242.121"
+    coi::cobbler_node { "compute-server01":
+      node_type     => "compute",
+      mac           => "11:22:33:44:55:66",
+      ip            => "192.168.242.21",
+      power_address => "192.168.242.121"
+    }
   }
 }
 
@@ -72,6 +75,7 @@ node /swift-storage\d+/ {
 # TODO - we are not sure what to do with this role. it is useful be able to boot up from scratch.
 # 
 #
+# TODO: check hiera's enable_cache here
 node /cache/ {
 
   include coi::roles::cache

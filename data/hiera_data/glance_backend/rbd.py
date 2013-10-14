567e0afbfc3f22a914164224adcafd70be202f10
Loic Dachary <loic@dachary.org>
2013-10-14 10:41:29 +0200

    add hiera_data for rbd in glance and cinder
    Signed-off-by: Loic Dachary <loic@dachary.org>

---
+++ data/hiera_data/glance_backend/rbd.py
@@ -0,0 +1,5 @@
+glance::backend::rbd::rbd_store_user: undef
+glance::backend::rbd::rbd_store_ceph_conf: '/etc/ceph/ceph.conf'
+glance::backend::rbd::rbd_store_pool: 'images'
+glance::backend::rbd::rbd_store_chunk_size: '8'
+
